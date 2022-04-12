0acbcd19e3b1254bca894a835b9a5981ccfe476e
ErwinIsmael <erwinlopez177@gmail.com>
2022-04-11 22:19:36 -0500

    quinto commit donde subo ejercicios de compilacion de programas con programacion funcional dadas en clase

---
+++ 03_Ejemplos_P_Funcional/Ejemplo_01_Con_parametros/LambdaTest.java
@@ -0,0 +1,15 @@
+  public class LambdaTest {
+    public static void main(String[] args){
+      // Expresion Lambda ==> representa un objeto de una interfaz funional
+      Operaciones op = (num1, num2) -> System.out.println(num1 + num2);
+
+      op.imprimeOperacion(5, 10);
+      
+      LambdaTest objeto = new LambdaTest();
+      objeto.miMetodo((num1, num2) -> System.out.println(num1 - num2), 20, 10);
+      objeto.miMetodo((num1, num2) -> System.out.println(num1 * num2), 20, 10);
+    }
+    public void miMetodo(Operaciones op, int num1, int num2){
+      op.imprimeOperacion(num1, num2);
+    }
+  }
