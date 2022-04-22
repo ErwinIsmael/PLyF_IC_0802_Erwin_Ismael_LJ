611638b70e096cb6b7efc902a7a3d92d8edc8263
ErwinIsmael <erwinlopez177@gmail.com>
2022-04-22 09:37:46 -0500

    Sexto commit, donde subo codigo que se le da solucion a la ambiguedad de expresiones lambda

---
+++ 04_Solucion_ambiguedad_lambda/Lambda_con_devolucion/LambdaTest.java
@@ -0,0 +1,22 @@
+public class LambdaTest{
+  public static void main(String[] args){
+    LambdaTest.engine((int x,int y) -> x + y );
+    LambdaTest.engine((int x, int y) -> x * y);
+    LambdaTest.engine((int x, int y) -> x / y);
+    LambdaTest.engine((int x, int y) -> x - y);
+    engine((int x, int y) -> x % y);
+  }
+
+  private static void engine(CalculadoraInt cal){
+    int x = 2, y = 4;
+    int resultado = cal.calcular(x, y);
+    System.out.println("resultado = " + resultado);
+  }
+
+  private static void engine(CalculadoraLong cal){
+    long x = 2, y = 4;
+    long resultado = cal.calcular(x, y);
+    System.out.println("resultado = " + resultado);
+
+  }
+}
